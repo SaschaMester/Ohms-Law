3a5dc0fe324106e90cdab5aaf9e8f34894b38920
Sascha Mester <sascha@debian.linux>
2014-10-06 03:41:05 +0200

    Committer: Sascha Mester <c_ur_free@yahoo.com> 	modified:   elec_de.py 	modified:   elec_en.py         Added charset encoding information

--- elec_de.py
+++ elec_de.py
@@ -1,4 +1,6 @@
 #! /usr/bin/python3 
+# -*- coding: utf-8 -*-
+
 from os import * # Wird benötigt für die Bildschirmlöschfunktion
 
 def wait():
--- elec_en.py
+++ elec_en.py
@@ -1,4 +1,6 @@
 #! /usr/bin/python3 
+# -*- coding: utf-8 -*-
+
 from os import * # Used for clear screen
 
 def wait():
